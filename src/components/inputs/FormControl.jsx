7933781ab15d97f98e61e762abdc1971a35408a4
Morphius22 <peaceandhealth@protonmail.com>
2023-11-16 20:33:22 -0800

    added a download resume button

--- src/components/inputs/FormControl.jsx
+++ src/components/inputs/FormControl.jsx
@@ -1,5 +1,6 @@
-import React from "react";
 import Button from "../Button";
+import jsPDF from "jspdf";
+import html2canvas from "html2canvas";
 
 const FormControl = ({
   clearResumeHandler,
@@ -8,6 +9,29 @@ const FormControl = ({
   setEducationExperiences,
   setPersonalDetails,
 }) => {
+  function downloadResumeAsPDF() {
+    const resumeElement = document.querySelector(".resume");
+    html2canvas(resumeElement).then((canvas) => {
+      const imgData = canvas.toDataURL("image/png");
+      const pdf = new jsPDF("p", "mm", "a4"); // create A4 portrait pdf
+      const width = pdf.internal.pageSize.getWidth();
+      const height = pdf.internal.pageSize.getHeight();
+      const aspectRatio = width / height;
+      const canvasAspectRatio = canvas.width / canvas.height;
+      let imgWidth;
+      let imgHeight;
+      if (canvasAspectRatio > aspectRatio) {
+        imgWidth = width;
+        imgHeight = imgWidth / canvasAspectRatio;
+      } else {
+        imgHeight = height;
+        imgWidth = imgHeight * canvasAspectRatio;
+      }
+      pdf.addImage(imgData, "PNG", 0, 0, imgWidth, imgHeight);
+      pdf.save("resume.pdf");
+    });
+  }
+
   return (
     <div className="formControl buttonGroup">
       <Button
@@ -31,6 +55,11 @@ const FormControl = ({
           })
         }
         className="btn btn-outline"
+      />
+      <Button
+        buttonText="Download Resume"
+        className="btn btn-outline"
+        onClick={downloadResumeAsPDF}
       />
     </div>
   );
